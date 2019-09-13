483db2f86a624563b876ba5a9bfeb1d7d3df1aec
abdoutelb <abdoutelb.fci@gmail.com>
2019-09-13 19:43:18 +0200

    upload jest files

---
+++ helpers.spec.js
@@ -0,0 +1,12 @@
+const helpers = require('./helpers');
+
+const isArabic = helpers.isArabic
+
+describe('isArabic', function() {
+  it('should be defined',function(){
+    expect(isArabic).toBeDefined();
+  })
+  it('should be a function', function() {
+    expect(isArabic).toBeInstanceOf(Function);
+  });
+});
