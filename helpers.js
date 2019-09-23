7416647fe37e9f955c079e016a1561fb454fb164
abdoutelb <abdoutelb.fci@gmail.com>
2019-09-23 22:24:10 +0200

    format and move function to helper

--- helpers.js
+++ helpers.js
@@ -1,48 +1,74 @@
-function isArabic(text) {
-    var pattern = /[\u0600-\u06FF\u0750-\u077F]/;
-    result = pattern.test(text);
-    return result;
-  }
+const fs = require("fs");
 
-  try {
-module.exports = {
-    isArabic : isArabic,
-      generateTasks : function (links){
-        let result = '';
-        for (let index = 1; index <= links.length; index++) {
-          result += `@task(${index})
-          def f${index}(self):
-           self.client.get("${links[index]}")
-               
-          `
-        }
-        return result;
-      },
-      generateFile :  function (tasks){
-        return `
-      from locust import HttpLocust, TaskSet, task
-      
-      
-      class UserBehavior(TaskSet):
-      
-          def fon_start(self):
-              self.login()
-      
-          def flogin(self):
-              self.client.get("/")
-      
-          
-          ${tasks}
-      
-      class WebsiteUser(HttpLocust):
-          task_set = UserBehavior
-          min_wait = 5000
-          max_wait = 9000
-      
-        `
-      }
+function isArabic(text) {
+  var pattern = /[\u0600-\u06FF\u0750-\u077F]/;
+  result = pattern.test(text);
+  return result;
 }
+
+function extractUrls(urls, ENV_URL) {
+  let cleanArray = urls.split("\n");
+  let usedLinks = [];
+  for (let index = 0; index < cleanArray.length; index++) {
+    if (cleanArray[index].startsWith(ENV_URL))
+      usedLinks.push(encodeURI(cleanArray[index].split(ENV_URL)[1]));
+  }
+  return usedLinks;
 }
-catch (error){
-console.log('We caught error');
-};
+
+try {
+  module.exports = {
+    isArabic: isArabic,
+    extractUrls: extractUrls,
+    extractFile: extractFile,
+    generateTasks: generateTasks,
+    generateFile: generateFile
+  };
+} catch (error) {
+  console.log("We caught error");
+}
+
+function extractFile(python) {
+  fs.writeFile("./loc.py", python, "utf8", function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("done !");
+  });
+}
+
+function generateTasks(links) {
+  let result = "";
+  for (let index = 1; index <= links.length; index++) {
+    result += `@task(${index})
+      def f${index}(self):
+       self.client.get("${links[index]}")
+           
+      `;
+  }
+  return result;
+}
+
+function generateFile(tasks) {
+  return `
+  from locust import HttpLocust, TaskSet, task
+  
+  
+  class UserBehavior(TaskSet):
+  
+      def fon_start(self):
+          self.login()
+  
+      def flogin(self):
+          self.client.get("/")
+  
+      
+      ${tasks}
+  
+  class WebsiteUser(HttpLocust):
+      task_set = UserBehavior
+      min_wait = 5000
+      max_wait = 9000
+  
+    `;
+}
--- index.js
+++ index.js
@@ -1,48 +1,21 @@
-const https = require('https');
-const url = process.env.URL;
-const fs = require('fs');
-const helpers = require('./helpers');
+const https = require("https");
+const ENV_URL = process.env.URL;
+const helpers = require("./helpers");
 
-https.get(`https://api.hackertarget.com/pagelinks/?q=${url}`, (resp) => {
-  let data = '';
+https
+  .get(`https://api.hackertarget.com/pagelinks/?q=${ENV_URL}`, resp => {
+    let data = "";
+    resp.on("data", chunk => {
+      data += chunk;
+    });
 
-
-  resp.on('data', (chunk) => {
-    data += chunk;
+    resp.on("end", () => {
+      let cleanUrls = helpers.extractUrls(data, ENV_URL);
+      let tasks = helpers.generateTasks(cleanUrls);
+      let pythonFile = helpers.generateFile(tasks);
+      helpers.extractFile(pythonFile);
+    });
+  })
+  .on("error", err => {
+    console.log("Error: " + err.message);
   });
-
-  resp.on('end', () => {
-    
-    let cleanUrls =  extractUrls(data)
-    let tasks = helpers.generateTasks(cleanUrls);
-    let pythonFile = helpers.generateFile(tasks);
-    extractFile(pythonFile);
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-
-
-function extractUrls(urls){
-    let cleanArray = urls.split("\n");
-    let usedLinks = []
-    for (let index = 0; index < cleanArray.length; index++) {
-      if(cleanArray[index].startsWith(url))
-      usedLinks.push(encodeURI(cleanArray[index].split(url)[1]))
-    }
-    return usedLinks;
-    } 
-        
-function extractFile(python){
-  
-fs.writeFile("./loc.py", python,'utf8', function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("done !");
-}); 
-}
-
-
-
