e9b4d3955e10a2eb9c1440dc23726babfab2095e
abdoutelb <abdoutelb.fci@gmail.com>
2019-10-27 20:56:11 +0200

    add env file & update docker compose

--- helpers.spec.js
+++ helpers.spec.js
@@ -1,12 +1,12 @@
-const helpers = require('./helpers');
+const helpers = require("./helpers");
 
-const isArabic = helpers.isArabic
+const isArabic = helpers.isArabic;
 
-describe('isArabic', function() {
-  it('should be defined',function(){
+describe("isArabic", function() {
+  it("should be defined", function() {
     expect(isArabic).toBeDefined();
-  })
-  it('should be a function', function() {
+  });
+  it("should be a function", function() {
     expect(isArabic).toBeInstanceOf(Function);
   });
 });
