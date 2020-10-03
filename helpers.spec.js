8c86de95c7401d82d18cd5f9c9b97cefe16f4d66
abdoutelb <abdoutelb.fci@gmail.com>
2020-10-04 00:15:38 +0200

    add tests for helper

--- helpers.spec.js
+++ helpers.spec.js
@@ -1,12 +1,38 @@
 const helpers = require("./helpers");
 
-const isArabic = helpers.isArabic;
 
-describe("isArabic", function() {
-  it("should be defined", function() {
-    expect(isArabic).toBeDefined();
+describe("extract urls from url", function () {
+  it("should be defined", function () {
+    expect(helpers.extractUrls).toBeDefined();
   });
-  it("should be a function", function() {
-    expect(isArabic).toBeInstanceOf(Function);
+  it("should be a function", function () {
+    expect(helpers.extractUrls).toBeInstanceOf(Function);
+  });
+});
+
+describe("read python file", function () {
+  it("should be defined", function () {
+    expect(helpers.extractFile).toBeDefined();
+  });
+  it("should be a function", function () {
+    expect(helpers.extractFile).toBeInstanceOf(Function);
+  });
+});
+
+describe("generate tasks for locust", function () {
+  it("should be defined", function () {
+    expect(helpers.generateTasks).toBeDefined();
+  });
+  it("should be a function", function () {
+    expect(helpers.generateTasks).toBeInstanceOf(Function);
+  });
+});
+
+describe("generate locust file", function () {
+  it("should be defined", function () {
+    expect(helpers.generateFile).toBeDefined();
+  });
+  it("should be a function", function () {
+    expect(helpers.generateFile).toBeInstanceOf(Function);
   });
 });
