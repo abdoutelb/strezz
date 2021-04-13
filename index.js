1d42fb1c8686aa8ba0936cf55b2940938b005a2f
abdoutelb <abdoutelb.fci@gmail.com>
2021-04-13 03:00:09 +0200

    solve deepsource warning

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const extracter = require("./extracter");
 
 (async () => {
   try {
-    let data = await extracter.extractLinks(ENV_URL);
-    let cleanUrls = helpers.extractUrls(data);
-    let tasks = helpers.generateTasks(cleanUrls);
-    let pythonFile = helpers.generateFile(tasks);
+    const data = await extracter.extractLinks(ENV_URL);
+    const cleanUrls = helpers.extractUrls(data);
+    const tasks = helpers.generateTasks(cleanUrls);
+    const pythonFile = helpers.generateFile(tasks);
     helpers.extractFile(pythonFile);
   } catch (e) {
     throw new Error(e);
